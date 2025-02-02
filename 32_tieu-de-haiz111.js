31669226d9fcc3967bfe4e3a8cc534d672b472f3
6gunlaZe <untilyouleesin@gmail.com>
2025-02-02 16:23:03 +0700

    Create 32_tieu-de-haiz111.js

---
+++ 32_tieu-de-haiz111.js
@@ -0,0 +1,629 @@
+// Hey there!
+// This is CODE, lets you control your character with code.
+// If you don't know how to code, don't worry, It's easy.
+// Just set attack_mode to true and ENGAGE!
+///VIPPPPPPPPPPPP
+//////////////// start_character("angioseal", 21); 
+// autorerun
+var idmap
+let delayitem
+let skillbua = 0
+//////////// move gan leader
+let started
+let started1
+let move1 = 0
+let framboss = 0
+let framboss1 = 0
+let delayboss
+let timemax = 7 * 60 * 1000
+let rateheal
+let stopgiudo = 0  // 1 = stop
+let run = 0
+let receivedData
+
+if (delayboss == undefined) delayboss = Date.now()
+
+
+setInterval(function() {
+
+ if (character.rip) { ///////auto hoi sinh
+    respawn();
+  }
+
+
+}, 50000);
+
+
+
+
+
+setInterval(function() {
+	    let leader = get_player("haiz");
+	
+	if(parent.S.icegolem && !leader && !get_nearest_monster({type:'icegolem'}))
+	{
+        join('icegolem');
+	}
+	
+	
+	if ( character.map == "winterland" && distance(character, {x: 800, y: 400}) < 250 && !smart.moving && !parent.S.icegolem  )use_skill("town")
+	
+	
+	
+	if (!character.party) {
+    send_party_request("haiz");
+	}
+
+	if (character.party && character.party != "haiz" ) {
+    leave_party();
+	}
+	
+
+	
+	if (leader && distance(character, leader) < 150) return
+    // Nếu nhân vật đang di chuyển, không làm gì thêm
+    if (smart.moving) return;
+
+	
+	
+    // Đảm bảo rằng nhận được thông tin hợp lệ
+    if (receivedData && typeof receivedData === 'object' && receivedData.message === "location") {
+        const targetMap = receivedData.map;  // Lấy tên bản đồ
+        const targetX = receivedData.x;      // Lấy tọa độ X
+        const targetY = receivedData.y;      // Lấy tọa độ Y
+
+        // Kiểm tra nếu nhân vật đang ở đúng bản đồ
+        if (character.map !== targetMap && character.map != "crypt") {
+            // Nếu không ở bản đồ mục tiêu, di chuyển đến bản đồ đó
+            smart_move({
+                map: targetMap,
+                x: targetX,
+                y: targetY
+            });
+        } else {
+            // Nếu đã ở đúng bản đồ, kiểm tra xem đã đến tọa độ mục tiêu chưa
+            if (character.x !== targetX || character.y !== targetY) {
+                // Nếu chưa đến, di chuyển đến tọa độ mới
+                xmove(targetX, targetY);
+            }
+        }
+    }
+
+}, 1000);
+
+
+///////////
+
+
+
+function on_magiport(name){
+    if(name == "nhiY"){
+        accept_magiport(name);
+    }
+}
+
+
+
+
+
+//////////////
+function on_cm(name, data) {
+
+
+	
+	
+	    if(name == "haiz"){
+
+		if (data == "goo" && character.map != "crypt")enter("crypt",idmap);
+
+	}
+	
+	 if(name == "haiz" && data != "goo" && typeof data === 'string' ){
+     idmap = data
+
+	}
+	
+	 if(name == "haiz" && data != "goo" ){
+     receivedData = data
+
+	}
+	
+	
+	
+	
+}
+
+
+
+
+
+
+
+function changeitem(options = {}) {
+	
+
+	if ( !options.slot ||  !options.name || !options.level ) return 
+	if (character.slots[options.slot])
+	{
+	if (character.slots[options.slot].name == options.name) return 
+	}	
+////////	
+	if (delayitem == undefined) delayitem = Date.now()
+	if (Date.now() < 300 + delayitem) return 
+	delayitem = Date.now()
+//////////		
+	
+	
+	let vitri = 100
+	
+	        for (let i = 0; i < character.isize; i++) {
+            const item = character.items[i]
+            if (!item) continue // No item in this slot
+
+            if (item.name == options.name && item.level == options.level) {
+                // This is an item we want to use!
+                    vitri = i //tim ra vi tri mon do
+            }
+				
+			}
+				
+	if (vitri == 100 ) return ///ko co mon do dung yeu cau
+	
+	
+	
+	
+	if (!character.slots[options.slot] || character.slots[options.slot].name != options.name) { 
+        unequip(options.slot);
+        equip(vitri, options.slot); 
+		 game_log("change: " + options.name)
+    }
+	
+
+//	changeitem({ slot: "mainhand", name : "firebow", level : 6 });
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function chuyendoithongminh(taget)
+{
+
+///////////////////////////////	tang dame
+
+	if ( skillbua == 1){
+		if (character.esize == 0){
+		 skillbua = 0
+			 game_log("fulll tui do !!!!!!");
+			return
+			
+		}
+		if (character.slots["offhand"])unequip( "offhand");
+
+        changeitem({ slot: "mainhand", name : "wbasher", level : 1 });
+		
+			if (character.slots["mainhand"] && character.slots["mainhand"].name == "wbasher")use_skill("stomp");
+				       
+	}
+	
+	
+		if ( skillbua == 1 &&  is_on_cooldown("stomp")){
+		          skillbua = 0
+
+	      }
+	
+		if ( skillbua == 0)
+		{
+changeitem({ slot: "mainhand", name : "fireblade", level : 9 });
+changeitem({ slot: "offhand", name : "fireblade", level : 8 });	
+		}
+	
+	
+	
+	
+//////////////////////////////
+}
+
+
+
+//////////////////////////////////////////////
+
+function kite(taget, kite_range)
+{
+
+	const radius = kite_range ;
+const  angle = Math.PI / 5.5 ;
+    if (can_move_to(taget.real_x, taget.real_y)) {
+        const angleFromCenterToCurrent = Math.atan2(character.y - taget.real_y, character.x - taget.real_x)
+        const endGoalAngle = angleFromCenterToCurrent + angle
+        const endGoal = { x: taget.real_x + radius * Math.cos(endGoalAngle), y: taget.real_y + radius * Math.sin(endGoalAngle) }
+        move(endGoal.x, endGoal.y)
+
+	
+	}
+ 
+	
+
+}
+
+
+
+function kiteSP(taget)
+{
+
+
+		//////////// dung skill
+	if(character.hp > 1500 && character.mp > 100 && !is_on_cooldown("taunt") && taget && taget.target != "haiz" )
+            {
+                use_skill("taunt", taget);
+				 game_log("phan no kite !!!!!!");
+            }	
+ 
+	
+	////
+		if(character.mp > 100 && !is_on_cooldown("charge") && taget )
+            {
+				   const dist1 = distance(character, taget);
+    if (dist1 >  character.range + 10)
+	{
+		                use_skill("charge");
+		 game_log("toc do kite !!!!!!");
+	}
+
+            }	
+ //////
+	////
+		if( character.hp > 12000 && character.mp > 840 && !is_on_cooldown("warcry") && taget )
+            {
+				   const dist1 = distance(character, taget);
+    if (dist1 > character.range)
+	{
+		use_skill("warcry");
+				 game_log("war !!!!!!");
+	}
+		
+	
+            }	
+ //////
+	 /////////////////////
+		if ( taget && !is_on_cooldown("hardshell") && taget.hp >5000 )
+	{
+		const dist1 = distance(character, taget);
+    if (dist1 <= character.range + 5 && taget.target == "haiz")
+	{
+		use_skill("hardshell");
+				 game_log("war hardshell !!!!!!");
+		if(is_moving(character))stop()
+	}
+		
+	}	
+
+      //////////////
+	
+//	 !currentTarget.s["cursed"]
+   	if( !character.s["hardshell"] && character.hp < 11000 && character.mp > 340 &&  skillbua == 0 &&  !is_on_cooldown("stomp") && taget){
+		skillbua = 1
+
+			}
+		
+
+
+	
+	///////////
+//	!character.s.hasOwnProperty("massproductionpp")
+
+
+///////////////////
+}
+
+
+
+function getBestTargets(options = {}) {
+    const entities = []
+
+    for (const id in parent.entities) {
+        const entity = parent.entities[id]
+        if (entity.type !== "monster") continue
+        if (entity.dead || !entity.visible) continue
+
+ if (options.max_range && distance(character, entity) > options.max_range) continue
+if (options.min_range && distance(character, entity) < options.min_range) continue
+ if (options.type && entity.mtype !== options.type) continue
+		 if (options.minHP && options.minHP*entity.max_hp > entity.hp) continue
+		
+		
+        entities.push(entity)
+    }
+
+
+    // We will return all entities, so that this function can be used with skills that target multiple entities in the future
+    return entities
+}
+
+
+
+
+
+
+
+
+function soloboss(options = {}) {
+    const entities = []
+     let number = 0
+	var bossarmy=["a0", "a1" , "a2" , "a3", "a4", "a5" , "a6" , "a7", "a8", "bat"]; 
+	
+    for (const id in parent.entities) {
+        const entity = parent.entities[id]
+        if (entity.type !== "monster") continue
+        if (entity.dead || !entity.visible) continue
+
+ if (options.max_range && distance(character, entity) > options.max_range) continue
+if (options.min_range && distance(character, entity) < options.min_range) continue
+ if (options.type && entity.mtype !== options.type) continue
+		 if (options.minHP && options.minHP*entity.max_hp > entity.hp) continue
+		 if (options.fullHP && entity.hp < entity.max_hp) continue
+		
+		if ( (bossarmy.indexOf(entity.mtype) == -1)   ) continue
+		////khong co trong list thi bo qua
+		// game_log(entity.mtype + distance(character, entity));
+		///
+		if ( options.number &&   (number+1) > options.number ) return entities;
+		/// lon hon so luong thi bo qua
+			number = 1 + number
+        entities.push(entity)
+    }
+
+
+    // We will return all entities, so that this function can be used with skills that target multiple entities in the future
+    return entities
+}
+
+	//////////check cac loai boss
+//  var  targetsoloboss = soloboss({ max_range: character.range, number : 1 })  //ham bo dem quai vat
+// targetsoloboss.length == 0	
+
+
+
+
+
+
+
+///////////////////////////////////////
+
+
+function use_hp_or_mp1()
+{
+	if(safeties && mssince(last_potion)<min(200,character.ping*3)) return resolving_promise({reason:"safeties",success:false,used:false});
+	var used=true;
+	if(is_on_cooldown("use_hp")) return resolving_promise({success:false,reason:"cooldown"});
+	
+	
+if (character.mp < 600 && character.hp > 2500 ) use_skill("use_mp");
+  else if (character.hp/character.max_hp< 0.97 && character.mp > 100 ) use_skill("use_hp");
+  else if (character.mp/character.max_mp < 0.75) use_skill("use_mp");
+
+	
+	else used=false;
+	if(used)
+		last_potion=new Date();
+	else
+		return resolving_promise({reason:"full",success:false,used:false});
+}
+
+
+var attack_mode= true
+
+setInterval(function(){
+
+	//use_hp_or_mp();
+	use_hp_or_mp1();
+	loot();
+	
+	if(character.s["hardshell"] && is_moving(character) ) stop();
+	
+	if(!attack_mode || character.rip ||  is_moving(character)) return;
+
+
+	
+	
+
+	
+///////////////////////////	
+////	khong move gan leader se khong tim muc tieu tan cong	
+const leader1 = get_player('haiz');
+const dist1 = distance(character, leader1);
+if (dist1 > 320)
+  return false;	
+/////////////////	
+
+
+//	if (leader1 && dist1 < 200 && leader1.hp > 5000  && character.mp > 300 && character.targets >= 1 && !is_on_cooldown("absorb")   )use_skill("absorb", "haiz");
+
+	    // Party leader
+    
+    
+    // Current target and target of leader.
+    var currentTarget = get_targeted_monster();
+    var leaderTarget = get_target_of(leader1)
+    
+    // Change the target.
+    if (!currentTarget || currentTarget != leaderTarget){ 
+        // Current target is empty or other than the leader's.
+        change_target(leaderTarget);
+        currentTarget = get_targeted_monster();
+    }
+	
+	
+////////////////////	
+chuyendoithongminh(currentTarget)	
+//kiteSP(currentTarget)	
+var f00 = get_player("haiz1")
+let rateskill
+if (currentTarget && currentTarget.attack >8000 )
+{
+	rateskill = 1.1
+}
+else
+{
+	rateskill = 0.85
+}
+
+if (currentTarget && f00 )VIPSuP(currentTarget,rateskill)		
+if (run == 1 && currentTarget && currentTarget.attack >8000 ) return //quai manh qua thi ne ra	
+if (skillbua == 1) return		 
+////////////////////
+
+//	if(!can_attack(currentTarget) && currentTarget && !character.s["hardshell"] )kite(currentTarget,character.range + 15);
+	
+
+	if( currentTarget && !is_in_range(currentTarget))
+	{
+		move(
+			character.x+(currentTarget.x-character.x)/2,
+			character.y+(currentTarget.y-character.y)/2
+			);
+		// Walk half the distance
+	}
+	else if(can_attack(currentTarget))
+	{
+
+		
+		/////////
+		set_message("Attacking");
+		attack(currentTarget);
+	}
+	
+	
+	
+
+
+
+
+	//////////////////////////////////////////////
+		
+	
+	
+	
+	
+	
+	
+	
+},1000/8); // Loops every 1/4 seconds.
+
+
+
+////////////
+setInterval(function() {
+
+skill_scare();
+
+}, 200);
+
+
+
+function skill_scare() {
+	
+if (is_on_cooldown("scare")) 
+{
+	changeitem({ slot: "orb", name : "orbg", level : 2 });
+}
+	
+if (character.targets == 0) {
+	return;
+}
+
+if (character.targets >= 1 && character.hp < 4000 && !is_on_cooldown("scare") &&  !character.s["hardshell"] ) 
+{
+	changeitem({ slot: "orb", name : "jacko", level : 2 });
+	use_skill("scare");
+	game_log("skill scare");
+
+}
+}
+
+
+
+
+
+function VIPSuP(taget,rate)
+{
+
+var f0 = get_player("haiz")
+var targetboss = taget	
+var supBOSS
+let safetank = 1
+let ratehp = rate	
+if (targetboss.target && targetboss.target == "haiz" && !f0.s["hardshell"] && distance(f0, targetboss) < (targetboss.range + 10) && !is_on_cooldown("taunt") && !is_on_cooldown("hardshell") && character.mp > 600 && f0.hp/f0.max_hp < ratehp ) use_skill("taunt", targetboss);
+
+if (targetboss.target && targetboss.target == "haiz" && !f0.s["hardshell"] && distance(f0, targetboss) < (targetboss.range + 10) && !is_on_cooldown("taunt") &&  character.s["hardshell"] && character.mp > 600 && character.hp > 7500 && f0.hp/f0.max_hp < ratehp ) use_skill("taunt", targetboss);		
+	
+if (targetboss.target && targetboss.target == "haiz1" && !character.s["hardshell"] && distance(character, targetboss) < (targetboss.range + 14) && !is_on_cooldown("hardshell") && character.mp > 500 && !targetboss.s["stunned"] && character.hp/character.max_hp < ratehp ) use_skill("hardshell");
+	
+if (targetboss.target && targetboss.target == "haiz1" && !character.s["hardshell"] && is_on_cooldown("hardshell") && targetboss.range <120) 
+{
+	kite(targetboss,targetboss.range + 50);
+	run = 1
+}
+	else
+	{
+		run = 0;
+	}
+	
+	
+	
+if (is_on_cooldown("hardshell") || character.mp < 550)safetank = 0	
+
+	
+if ( f0.hp/f0.max_hp < ratehp )	
+{	
+if ( targetboss.target && targetboss.target == "haiz1" && !character.s["hardshell"] && character.mp > 200 && !is_on_cooldown("stomp") && distance(character, targetboss) < (targetboss.range + 7) && is_on_cooldown("hardshell") )skillbua = 1
+	
+if (character.mp > 900 && is_on_cooldown("hardshell") && !is_on_cooldown("stomp") && !f0.s["hardshell"] && !targetboss.s["stunned"] && distance(f0, targetboss) < (targetboss.range + 20))skillbua = 1
+	
+if ( character.mp > 1200 && !is_on_cooldown("stomp") && !targetboss.s["stunned"] && !f0.s["hardshell"] && !character.s["hardshell"] ) skillbua = 1	
+}		
+	
+	
+if (run == 1 && supBOSS )	
+{
+			var target3 = get_nearest_monster({type: supBOSS});
+             if(target3 && is_in_range(target3))attack(target3);
+}		
+	
+	
+
+	
+///////////////////////////////////////////////////////////	
+//////////////////////////////////////////////////////////////
+
+		if(character.mp > 100 && !is_on_cooldown("charge") && run == 1 && safetank == 0 )use_skill("charge");
+
+
+
+if( character.mp > 840 && !is_on_cooldown("warcry") && taget &&  !character.s["warcry"] )use_skill("warcry");
+
+
+	///////////
+//	!character.s.hasOwnProperty("massproductionpp")
+
+
+///////////////////
+}
+
+
+
+// Learn Javascript: https://www.codecademy.com/learn/introduction-to-javascript
+// Write your own CODE: https://github.com/kaansoral/adventureland
+// NOTE: If the tab isn't focused, browsers slow down the game
+// NOTE: Use the performance_trick() function as a workaround
