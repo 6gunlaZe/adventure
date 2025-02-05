6bcd19a82581d638da48aeff82e021d953102bb8
6gunlaZe <untilyouleesin@gmail.com>
2025-02-05 19:01:22 +0700

    Update 38_haiz_auto_cryts1.js

--- 38_haiz_auto_cryts1.js
+++ 38_haiz_auto_cryts1.js
@@ -10,7 +10,7 @@ let skillbua = 0
 let run = 1
 let datahero
 
-game_log("Game vs 1.9");
+game_log("Game vs 1.0");
 
 setInterval(function() {
 
@@ -62,8 +62,8 @@ let toado = [
 ];
 
 
- var  targetkill = solobosskill({ max_range: 400}) 
- var  targetNO = solobossNO({ max_range: 400}) 
+ var  targetkill = solobosskill({ max_range: 320}) 
+ var  targetNO = solobossNO({ max_range: 330}) 
    
 game_log("checkk boss can kill !!!!!!  "+ targetkill.length   );	
 game_log("checkk boss NO kill!!!!!!  "+  targetNO.length  );	
