ecf8ed2a8ffac55bcd71c809debe34539c915d10
6gunlaZe <untilyouleesin@gmail.com>
2025-02-12 23:24:45 +0700

    Update 7_haiz-fram.js

--- 7_haiz-fram.js
+++ 7_haiz-fram.js
@@ -6,7 +6,7 @@
 // Just set attack_mode to true and ENGAGE!
 ///VIPPPPPPPPPPPP
 ////////////////
-game_log("Game vs 1.3");
+game_log("Game vs 1.5");
 let host
 if (character.id == "haiz") host = 1
 else host =0
