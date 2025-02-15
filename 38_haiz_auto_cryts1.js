ad8f90b430c603d50ab530b4e8846414adaaf212
6gunlaZe <untilyouleesin@gmail.com>
2025-02-15 22:03:25 +0700

    Update 38_haiz_auto_cryts1.js

--- 38_haiz_auto_cryts1.js
+++ 38_haiz_auto_cryts1.js
@@ -224,7 +224,7 @@ game_log("ZZZ = !!!!!!  "+ z  );
 		if (z=0)z = 1
 		else if (z=1)z = 2
 		else if (z=2)z = 1
-                   if (result) {
+                   if (result && z <3) {
                   xmove(result.x, result.y);  // Di chuyển tới vị trí (x, y)
                  }
 		return
