bfe61c762426aedf5f643b13afd31b0e2688bea6
6gunlaZe <untilyouleesin@gmail.com>
2025-02-17 18:29:53 +0700

    Update bot_telegram.js

--- bot_telegram.js
+++ bot_telegram.js
@@ -1,3 +1,6 @@
+
+let codemode = 0
+
 ///////////////
 const botToken = '7892397096:AAH7nDreQHQ9fPcsMJNi8MIRwZEDPQzFPgc'; // Thay YOUR_BOT_TOKEN bằng token của bạn
 
@@ -143,6 +146,7 @@ function handleCallbackQuery(callbackQuery) {
   // Nếu thời gian quá lâu (10 giây), bỏ qua xử lý
   if (timeDiff > callbackTimeout) {
     console.log('Callback query expired. Skipping...');
+	        sendSyntaxExamples(chatId);
     return;  // Bỏ qua callback query nếu thời gian quá lâu
   }
 
@@ -171,16 +175,20 @@ function performTask(key, data, chatId) {
 
   if (key === 'reset') {
     console.log('Thực hiện reset!');
+	  parent.api_call("disconnect_character", {name: "haiz"});
     sendMessage(chatId, 'Nhiệm vụ reset đã hoàn thành!');
   } else if (key === 'fram') {
     console.log('Thực hiện fram!');
-    sendMessage(chatId, 'Nhiệm vụ fram đã hoàn thành!');
+                respawn()
+	  sendMessage(chatId, 'Nhiệm vụ fram đã hoàn thành!');
   } else if (key === 'bank') {
     console.log('Thực hiện bank!');
     sendMessage(chatId, 'Nhiệm vụ bank đã hoàn thành!');
   } else if (key === 'crypt') {
     console.log('Thực hiện crypt!');
-    sendMessage(chatId, 'Nhiệm vụ crypt đã hoàn thành!');
+                codemode = 1
+	  	        Key.push(data); 
+	  sendMessage(chatId, 'Nhiệm vụ crypt đã hoàn thành!');
   } else {
     console.log('Không có nhiệm vụ xác định cho key:', key);  // Debug log: Kiểm tra trường hợp không có nhiệm vụ
     sendMessage(chatId, `Không có nhiệm vụ xác định cho key: ${key}`);
@@ -189,6 +197,130 @@ function performTask(key, data, chatId) {
 
 // Khởi động bot sau khi chờ 30 giây
 setTimeout(() => {
-  console.log('Bot is now starting...');
+  sendMessage(6708647498, 'Bot is now starting...!');
+	sendSyntaxExamples(6708647498);
   getUpdates(); // Gọi hàm getUpdates lần đầu tiên
-}, initialDelay);
+}, 2000);
+
+
+
+
+
+setTimeout(() => {
+if (codemode == 0){ 
+	load_code(7)
+	  sendMessage(6708647498, 'Bot is haiz fram!');
+
+}
+	else if (codemode == 1) {
+		load_code(38)
+		  sendMessage(6708647498, 'Bot is crypt!');
+
+	}
+}, 8000);
+
+
+
+let curenkey
+let Key = [];
+
+
+
+
+setInterval(function() {
+if (codemode != 1 )return
+ let member1 = get_player("haiz1");
+ let member2 = get_player("Ynhi");
+	
+if ( character.map == "cave" && distance(character, {x: -194, y: -1281}) < 50 && member1 && member2 ){
+
+if (Key.length > 0) {
+     curenkey = Key.shift();  // Xóa phần tử đầu tiên và lưu lại phần tử đó
+	enter("crypt",curenkey);
+
+} else {
+	if ( soluongitem("cryptkey") >= 1 )enter("crypt");
+	else parent.api_call("disconnect_character", {name: "haiz"});
+}
+
+}	
+	
+	
+}, 5000);
+
+
+
+
+function soluongitem(name)
+{
+	let sl = 0
+	//Iterate over every slot in our inventory.
+	for(var i = 0; i <= 41; i++)
+	{
+		var curSlot = character.items[i];
+		
+		//Does the item name match?
+		if(curSlot != null && curSlot.name == name)
+		{
+			sl += curSlot.q
+		}
+	}
+	
+	return sl
+}
+
+
+
+
+
+
+
+
+
+
+
+
+/////////////////////////////////////////////////////
+let isActive = true;
+
+setTimeout(() => {
+  isActive = false; // Tắt hành động sau 20 giây
+  console.log('Bot stopped after 20 seconds');
+}, 20000); // Dừng hành động sau 20 giây
+
+function hpp() {
+  if (isActive) {
+    console.log('Fetching updates...');
+    // Thực hiện các hành động cập nhật ở đây
+	  
+if (character.hp/character.max_hp< 0.8 && character.mp > 100 ) {
+   use_skill("use_hp");
+} 
+else if (character.mp/character.max_mp< 0.8) {
+	use_skill("use_mp");
+}
+  else if (character.max_mp>character.mp) return use_skill("regen_mp");
+	
+	  
+    // Bạn có thể gọi lại getUpdates() nếu cần để cập nhật liên tục trong 20 giây
+    setTimeout(getUpdates, 300); // Gọi lại getUpdates() mỗi giây nếu chưa dừng
+  } else {
+    console.log('Bot has stopped updating');
+  }
+}
+
+hpp(); // Bắt đầu thực hiện () ngay khi mở trang
+
+
+//////////////////////////////////////////////////////
+
+
+
+
+
+
+
+
+
+
+
