d7c66de9077aec25b7ccece8ed56c42283e8ad4f
6gunlaZe <untilyouleesin@gmail.com>
2025-02-17 23:20:09 +0700

    Update 7_haiz-fram.js

--- 7_haiz-fram.js
+++ 7_haiz-fram.js
@@ -385,7 +385,7 @@ function on_draw(){
   if(draw_debug){
       clear_drawings();
 /////////
-	  if(character.s["sugarrush"] || delay == 0 )
+	  if(character.s["sugarrush"] )
 	  {
 	  
 		  loop = 20
