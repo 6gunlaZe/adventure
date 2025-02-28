3cc562dd50daf4bc4e370ee32048b2bcc4ae9c75
6gunlaZe <untilyouleesin@gmail.com>
2025-02-28 19:19:02 +0700

    Update 12_Boss-nhiY.js

--- 12_Boss-nhiY.js
+++ 12_Boss-nhiY.js
@@ -187,16 +187,16 @@ if (checkbat == 1) {
 if (step >= 4) {
     godenbat = 0;
 }	
-game_log(step)	
-if (step = 1 && checkbat == 0 && runb == 0){
+//game_log(step)	
+if (step == 1 && checkbat == 0 && runb == 0){
 	smart_move({ map: "cave", x: 1154, y: 55 })	
 runb = 1    
 	     }
-if (step = 2 && checkbat == 0 && runb == 0){
+if (step == 2 && checkbat == 0 && runb == 0){
 	smart_move({ map: "cave", x: -261, y: -454 })	
 runb = 1
 	     }
-if (step = 3 && checkbat == 0 && runb == 0){
+if (step == 3 && checkbat == 0 && runb == 0){
 	smart_move({ map: "cave", x: 325, y: -1118 })	
 runb = 1
 	     }	
