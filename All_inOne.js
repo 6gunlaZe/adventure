88acf522f068f8bf6ba500890d278d0d1ab82347
6gunlaZe <untilyouleesin@gmail.com>
2025-03-12 23:57:15 +0700

    Update All_inOne.js

--- All_inOne.js
+++ All_inOne.js
@@ -282,7 +282,7 @@ async function attackLoop() {
             }
         }
 
-    var  targetsoloboss = NOTsoloboss({ max_range: 200, number : 1 }) 
+    var  targetsoloboss = NOTsoloboss1({nhonhat: 1});
     if ( !nearest && events && targetsoloboss.length == 1)nearest = targetsoloboss;
 	    
     var  targetsoloboss1 = soloboss({ max_range: 300, number : 1 }) 
@@ -1406,6 +1406,54 @@ if (options.min_range && distance(character, entity) < options.min_range) contin
     // We will return all entities, so that this function can be used with skills that target multiple entities in the future
     return entities
 }
+
+
+
+
+function NOTsoloboss1(options = {}) {
+	//var target1xc= get_nearest_monster1({type: crepp,  nhonhat: 1});
+        let checkkill = 0
+	var min_d=character.range ,target=null;
+		let hpp = 10000000000000
+        var bossarmy=["icegolem", "franky" , "crabxx" ]; 
+	if(!args) args={};
+	if(args && args.target && args.target.name) args.target=args.target.name;
+	if(args && args.type=="monster") game_log("get_nearest_monster: you used monster.type, which is always 'monster', use monster.mtype instead");
+	if(args && args.mtype) game_log("get_nearest_monster: you used 'mtype', you should use 'type'");
+
+	for(id in parent.entities)
+	{
+		var current=parent.entities[id];
+		if(current.type!="monster" || !current.visible || current.dead) continue;
+		if(args.type && current.mtype!=args.type) continue;
+		if(args.min_xp && current.xp<args.min_xp) continue;
+		if(args.max_att && current.attack>args.max_att) continue;
+		if(args.target && current.target!=args.target) continue;
+		if(args.no_target && current.target && current.target!=character.name) continue;
+		if(args.NO_target && current.target) continue;
+		if(args.path_check && !can_move_to(current)) continue;
+		if ( (bossarmy.indexOf(current.mtype) == -1)   ) continue
+		checkkill = get_nearest_playerV_noMyparty(entity)
+	        if (checkkill < 3)continue
+		var c_dist=parent.distance(character,current);
+		if (c_dist>min_d) continue;
+		if(args.cus && !current.s["cursed"]  )continue;//co debuff thi chon
+		if(args.nhonhat && current.hp > hpp)continue;//lua chon hp nho nhat
+		hpp = current.hp
+		if(c_dist<min_d) min_d=c_dist,target=current; //lua chon quai vat gan nhat
+	}
+	return target;
+}
+
+
+
+
+
+
+
+
+
+
 
 
 
