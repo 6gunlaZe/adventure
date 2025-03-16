e601d93b164b79a9cb4436a68b9745fdc69712a0
6gunlaZe <untilyouleesin@gmail.com>
2025-03-17 01:30:02 +0700

    Update 12_Boss-nhiY.js

--- 12_Boss-nhiY.js
+++ 12_Boss-nhiY.js
@@ -106,7 +106,7 @@ if (lastMain &&  character.map == map && distance(character, {x: x, y: y}) < 30)
 
 ///////smarrtmove
 const congdichuyen = findcongdichchuyen(checkdichuyen);
-if(!smart.moving && congdichuyen != 1)transport(congdichuyen.map,congdichuyen.s);	
+if(congdichuyen != 1 && distance(character, {x: lastMain.x, y: lastMain.y}) < 30 )transport(congdichuyen.map,congdichuyen.s);	
 //if(!smart.moving && congdichuyen != 1)smart_move({ map: congdichuyen.map, x: congdichuyen.x, y: congdichuyen.y })
  if (character.moving || smart.moving) return
 if (character.mp > 2800 && foxmode == 1 && Object.keys(Savedatasmart).length == 0 && get_nearest_monster({type: "phoenix"}) ){
