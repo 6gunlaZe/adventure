a252f6c3783a4ea943324419aa0ad5fdc4f2e494
6gunlaZe <untilyouleesin@gmail.com>
2025-03-17 01:57:30 +0700

    Update 12_Boss-nhiY.js

--- 12_Boss-nhiY.js
+++ 12_Boss-nhiY.js
@@ -101,14 +101,14 @@ for (let i = 0; i < checkdichuyen.plot.length; i++) {
 
 if (lastMain && character.mp > 1800 && distance(character, {x: lastMain.x, y: lastMain.y}) > 150) {
 	use_skill("blink", [lastMain.x, lastMain.y])
-if(lastMain.x == x && lastMain.y == y && character.map == map)stop()
-if (lastMain &&  character.map == map && distance(character, {x: x, y: y}) < 30)stop()	
+const congdichuyen = findcongdichchuyen(checkdichuyen);
+if(lastMain && congdichuyen != 1 && distance(character, {x: lastMain.x, y: lastMain.y}) < 30 )transport(congdichuyen.map,congdichuyen.s);	
+	stop()
 }
 
 
 ///////smarrtmove
-const congdichuyen = findcongdichchuyen(checkdichuyen);
-if(lastMain && congdichuyen != 1 && distance(character, {x: lastMain.x, y: lastMain.y}) < 30 )transport(congdichuyen.map,congdichuyen.s);	
+
 //if(!smart.moving && congdichuyen != 1)smart_move({ map: congdichuyen.map, x: congdichuyen.x, y: congdichuyen.y })
  if (character.moving || smart.moving) return
 if (character.mp > 2800 && foxmode == 1 && Object.keys(Savedatasmart).length == 0 && get_nearest_monster({type: "phoenix"}) ){
