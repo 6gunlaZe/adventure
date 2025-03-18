81fdb9e310c1649c25d98a998f64bd6393f235af
6gunlaZe <untilyouleesin@gmail.com>
2025-03-18 20:26:24 +0700

    Update ALLinOne_6gunlaZe_fram.js

--- ALLinOne_6gunlaZe_fram.js
+++ ALLinOne_6gunlaZe_fram.js
@@ -467,6 +467,58 @@ function get_nearest_monster_v2(args = {}) {
 }
 
 
+function handleSnowball() {
+	if(character.map != "crypt")return
+  if (can_use("snowball")) {
+    const currentTime = new Date().getTime(); // Lấy thời gian hiện tại (ms)
+    
+    for (const id in parent.entities) {
+      const entity = parent.entities[id];
+      
+      // Kiểm tra loại quái vật
+      if (entity.type !== "monster") continue;
+      
+      // Kiểm tra xem quái vật có chết hoặc không thể thấy không
+      if (entity.dead || !entity.visible) continue;
+      
+      // Kiểm tra mtype của quái vật có phải là "goo" không
+      if (entity.mtype !== "goo") continue;
+      
+      // Kiểm tra quái vật có bị đóng băng không
+      if (entity.s["frozen"]) continue;
+      
+      // Kiểm tra khoảng cách với quái vật có lớn hơn 200 không
+      if (distance(character, entity) > 200) continue;
+      
+      // Kiểm tra xem quái vật đã bị bắn tuyết
+      if (entity.snowballed) {
+        // Kiểm tra thời gian debuff đã hết 5 giây chưa
+        if (currentTime - entity.snowballedTime > 3700) {
+          // Debuff đã hết, cho phép bắn lại
+          entity.snowballed = false; // Xóa cờ snowballed
+        } else {
+          // Nếu debuff chưa hết, bỏ qua
+          continue;
+        }
+      }
+      
+      // Sử dụng kỹ năng snowball vào quái vật
+      use_skill("snowball", entity);
+      
+      // Đánh dấu quái vật là đã bị bắn tuyết và lưu thời gian bắn tuyết
+      entity.snowballed = true;
+      entity.snowballedTime = currentTime; // Lưu thời gian bắn tuyết (ms)
+      
+      // Dừng vòng lặp sau khi sử dụng kỹ năng vào một quái vật
+      break;
+    }
+  }
+}
+
+// Gọi hàm `handleSnowball` mỗi 100ms
+setInterval(handleSnowball, 100);
+
+
 
 
 
@@ -963,7 +1015,7 @@ setInterval(function() {
     }
 
 
-    let itemsToExclude = ["hpot0", "mpot0","hpot1", "mpot1", "elixirint0","elixirstr0","elixirdex0","elixirint1","elixirstr1","elixirdex1", "luckbooster", "goldbooster", "xpbooster", "pumpkinspice", "xptome","cscroll0", "cscroll1", "scroll0", "scroll1", "tracker","crossbow","jacko", "pouchbow","orbg"];
+    let itemsToExclude = ["snowball","hpot0", "mpot0","hpot1", "mpot1", "elixirint0","elixirstr0","elixirdex0","elixirint1","elixirstr1","elixirdex1", "luckbooster", "goldbooster", "xpbooster", "pumpkinspice", "xptome","cscroll0", "cscroll1", "scroll0", "scroll1", "tracker","crossbow","jacko", "pouchbow","orbg"];
 	
 	
     for (let i = 0; i < 42; i++) {
