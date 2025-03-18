ca6c9ca51746597c04c5d3ebe4e928c3a606a150
6gunlaZe <untilyouleesin@gmail.com>
2025-03-18 20:43:04 +0700

    Update 28_2-buff-main.js

--- 28_2-buff-main.js
+++ 28_2-buff-main.js
@@ -9,7 +9,7 @@ var idmap
 let rateheal
 let delayitem
 let stopgiudo = 0  // 1 = stop
-
+let cryts = 0
 let receivedData
 
 if (delayboss == undefined) delayboss = Date.now()
@@ -33,6 +33,11 @@ setInterval(function() {
     send_party_request("haiz");
 	}
 
+if (character.map != "crypt")
+{
+cryts = 0	
+
+	
 	if (character.party && character.party != "haiz" ) {
     leave_party();
 	}
@@ -74,6 +79,11 @@ setInterval(function() {
             }
         }
     }
+}
+	else
+{
+cryts = 1	
+}
 
 }, 1000);
 
@@ -654,6 +664,270 @@ if (character.id == "Ynhi")changeitem({ slot: "mainhand", name : "oozingterror",
 }
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+////////////////////////////////////////////////////////////////////////////////
+// Extra range to add to a monster's attack range to give more wiggle room
+const rangeBuffer = 60;
+
+// How far away we want to consider monsters
+const calcRadius = 300;
+
+// Types of monsters we want to avoid
+const avoidTypes = ["a0","a1","a2","a4","a6","a8","a9","a10"];
+
+const avoidPlayers = false; // Set to false to not avoid players at all
+const playerBuffer = 0; // Additional range around players
+const avoidPlayersWhitelist = []; // Players to avoid differently
+const avoidPlayersWhitelistRange = 30; // Set to null to not avoid whitelisted players
+const playerRangeOverride = 3; // Overrides how far to avoid, set to null to use player range
+const playerAvoidIgnoreClasses = ["merchant"]; // Classes we don't want to avoid
+
+// Tracking when we send movements to avoid flooding the socket
+let lastMove;
+
+// Whether we want to draw the various calculations visually
+const drawDebug = false;
+
+
+function avoidance() {
+	if (cryts == 0)return
+    if (drawDebug) {
+        clear_drawings();
+    }
+
+    // Try to avoid monsters
+    const avoiding = avoidMobs();
+
+    if (!avoiding) {
+        if ((!lastMove || new Date() - lastMove > 100)  && cryts > 0) {
+		let host = get_player("haiz")
+           if(host && !smart.moving )xmove(host.real_x, host.real_y); // Move to current position (no goal used)
+            lastMove = new Date();
+        }
+    }
+
+}
+setInterval(avoidance, 50);
+
+function avoidMobs() {
+    let maxWeight = -Infinity;
+    let maxWeightAngle = 0;
+
+    const monstersInRadius = getMonstersInRadius();
+    const avoidRanges = getAnglesToAvoid(monstersInRadius);
+    const inAttackRange = isInAttackRange(monstersInRadius);
+
+    // If we are in attack range or need to avoid monsters, find the safest direction to move
+    if (inAttackRange || (!can_move_to(character.real_x, character.real_y))) {
+        for (let angle = 0; angle < Math.PI * 2; angle += Math.PI / 60) {
+            let weight = 0;
+
+            const position = pointOnAngle(character, angle, 75);
+
+            if (can_move_to(position.x, position.y)) {
+                let rangeWeight = 0;
+                let inRange = false;
+
+                for (const id in monstersInRadius) {
+                    const entity = monstersInRadius[id];
+                    const monsterRange = getRange(entity);
+                    const distToMonster = distanceToPoint(position.x, position.y, entity.real_x, entity.real_y);
+                    const charDistToMonster = distanceToPoint(character.real_x, character.real_y, entity.real_x, entity.real_y);
+
+                    if (charDistToMonster < monsterRange) {
+                        inRange = true;
+                        if (distToMonster > charDistToMonster) {
+                            rangeWeight += distToMonster - charDistToMonster;
+                        }
+                    }
+                }
+
+                if (inRange) {
+                    weight = rangeWeight;
+                }
+
+                const intersectsRadius = angleIntersectsMonsters(avoidRanges, angle);
+                if (!can_move_to(character.real_x, character.real_y)) {
+                    weight -= distanceToPoint(position.x, position.y, character.real_x, character.real_y) / 10;
+                }
+
+                if (!intersectsRadius) {
+                    if (weight > maxWeight) {
+                        maxWeight = weight;
+                        maxWeightAngle = angle;
+                    }
+                }
+            }
+        }
+
+        const movePoint = pointOnAngle(character, maxWeightAngle, 20);
+        if (!lastMove || new Date() - lastMove > 100) {
+            lastMove = new Date();
+            move(movePoint.x, movePoint.y);
+        }
+
+        if (drawDebug) {
+            draw_line(character.real_x, character.real_y, movePoint.x, movePoint.y, 2, 0xF20D0D);
+        }
+
+        return true;
+    }
+
+    return false;
+}
+
+function getRange(entity) {
+    if (entity.type !== "character") {
+        return (parent.G.monsters[entity.mtype]?.range || 100) + rangeBuffer;
+    } else {
+        if (avoidPlayersWhitelist.includes(entity.id) && avoidPlayersWhitelistRange != null) {
+            return avoidPlayersWhitelistRange;
+        } else if (playerRangeOverride != null) {
+            return playerRangeOverride + playerBuffer;
+        } else {
+            return (entity.range || 100) + playerBuffer;
+        }
+    }
+}
+
+function isInAttackRange(monstersInRadius) {
+    return monstersInRadius.some(monster => {
+        const monsterRange = getRange(monster);
+        const charDistToMonster = distanceToPoint(character.real_x, character.real_y, monster.real_x, monster.real_y);
+        return charDistToMonster < monsterRange;
+    });
+}
+
+function angleIntersectsMonsters(avoidRanges, angle) {
+    return avoidRanges.some(range => isBetween(range[1], range[0], angle));
+}
+
+function getAnglesToAvoid(monstersInRadius) {
+    const avoidRanges = [];
+    for (const id in monstersInRadius) {
+        const monster = monstersInRadius[id];
+        const monsterRange = getRange(monster);
+        const tangents = findTangents({ x: character.real_x, y: character.real_y }, { x: monster.real_x, y: monster.real_y, radius: monsterRange });
+
+        if (!isNaN(tangents[0].x)) {
+            const angle1 = angleToPoint(character, tangents[0].x, tangents[0].y);
+            const angle2 = angleToPoint(character, tangents[1].x, tangents[1].y);
+            avoidRanges.push(angle1 < angle2 ? [angle1, angle2] : [angle2, angle1]);
+
+            if (drawDebug) {
+                draw_line(character.real_x, character.real_y, tangents[0].x, tangents[0].y, 1, 0x17F20D);
+                draw_line(character.real_x, character.real_y, tangents[1].x, tangents[1].y, 1, 0x17F20D);
+            }
+        }
+
+        if (drawDebug) {
+            draw_circle(monster.real_x, monster.real_y, monsterRange, 1, 0x17F20D);
+        }
+    }
+    return avoidRanges;
+}
+
+function getMonstersInRadius() {
+    return Object.values(parent.entities).filter(entity => {
+        const distanceToEntity = distanceToPoint(entity.real_x, entity.real_y, character.real_x, character.real_y);
+        const range = getRange(entity);
+        return (entity.type === "monster" && avoidTypes.includes(entity.mtype) && distanceToEntity < calcRadius) ||
+            (avoidPlayers && entity.type === "character" && !entity.npc && !playerAvoidIgnoreClasses.includes(entity.ctype) &&
+                (!avoidPlayersWhitelist.includes(entity.id) || avoidPlayersWhitelistRange != null) &&
+                (distanceToEntity < calcRadius || distanceToEntity < range));
+    });
+}
+
+function normalizeAngle(angle) {
+    return Math.atan2(Math.sin(angle), Math.cos(angle));
+}
+
+function isBetween(angle1, angle2, target) {
+    if (angle1 <= angle2) {
+        return angle2 - angle1 <= Math.PI ? angle1 <= target && target <= angle2 : angle2 <= target || target <= angle1;
+    } else {
+        return angle1 - angle2 <= Math.PI ? angle2 <= target && target <= angle1 : angle1 <= target || target <= angle2;
+    }
+}
+
+function findTangents(point, circle) {
+    const dx = circle.x - point.x;
+    const dy = circle.y - point.y;
+    const dd = Math.sqrt(dx * dx + dy * dy);
+    const a = Math.asin(circle.radius / dd);
+    const b = Math.atan2(dy, dx);
+
+    const ta = { x: circle.x + circle.radius * Math.sin(b - a), y: circle.y - circle.radius * Math.cos(b - a) };
+    const tb = { x: circle.x - circle.radius * Math.sin(b + a), y: circle.y + circle.radius * Math.cos(b + a) };
+
+    return [ta, tb];
+}
+
+function offsetToPoint(x, y) {
+    const angle = angleToPoint(x, y) + Math.PI / 2;
+    return angle - characterAngle();
+}
+
+function pointOnAngle(entity, angle, distance) {
+    return { x: entity.real_x + distance * Math.cos(angle), y: entity.real_y + distance * Math.sin(angle) };
+}
+
+function entityAngle(entity) {
+    return (entity.angle * Math.PI) / 180;
+}
+
+function angleToPoint(entity, x, y) {
+    const deltaX = entity.real_x - x;
+    const deltaY = entity.real_y - y;
+    return Math.atan2(deltaY, deltaX) + Math.PI;
+}
+
+function characterAngle() {
+    return (character.angle * Math.PI) / 180;
+}
+
+function distanceToPoint(x1, y1, x2, y2) {
+    return Math.sqrt(Math.pow(x1 - x2, 2) + Math.pow(y1 - y2, 2));
+}
 // Learn Javascript: https://www.codecademy.com/learn/introduction-to-javascript
 // Write your own CODE: https://github.com/kaansoral/adventureland
 // NOTE: If the tab isn't focused, browsers slow down the game
