a1b42975dbdc67cd934526a23eda9ec5cf8ef214
6gunlaZe <untilyouleesin@gmail.com>
2025-03-20 22:04:03 +0700

    Update ALLinOne_6gunlaZe_fram.js

--- ALLinOne_6gunlaZe_fram.js
+++ ALLinOne_6gunlaZe_fram.js
@@ -20,7 +20,7 @@ const locations = {
     gscorpion: [{ x: 390, y: -1422 }],
     iceroamer: [{ x: 823, y: -45 }],
     mechagnome: [{ x: 0, y: 0 }],
-    mole: [{ x: 4, y: -282 }],
+    mole: [{ x: 4, y: -762 }],  //-282
     mummy: [{ x: 256, y: -1417 }],
     oneeye: [{ x: -270, y: 160 }],
     pinkgoblin: [{ x: 366, y: 377 }],
