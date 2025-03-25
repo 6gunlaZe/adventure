7621450a987b17aab3e83e274b3b830dc09c367f
6gunlaZe <untilyouleesin@gmail.com>
2025-03-25 18:59:30 +0700

    Update ALLinOne_6gunlaZe_fram.js

--- ALLinOne_6gunlaZe_fram.js
+++ ALLinOne_6gunlaZe_fram.js
@@ -1077,7 +1077,7 @@ setInterval(function() {
     }
 
 
-    let itemsToExclude = ["snowball","hpot0", "mpot0","hpot1", "mpot1", "elixirint0","elixirstr0","elixirdex0","elixirint1","elixirstr1","elixirdex1", "luckbooster", "goldbooster", "xpbooster", "pumpkinspice", "xptome","cscroll0", "cscroll1", "scroll0", "scroll1", "tracker","crossbow","jacko", "pouchbow","orbg"];
+    let itemsToExclude = ["mittens","supermittens","snowball","hpot0", "mpot0","hpot1", "mpot1", "elixirint0","elixirstr0","elixirdex0","elixirint1","elixirstr1","elixirdex1", "luckbooster", "goldbooster", "xpbooster", "pumpkinspice", "xptome","cscroll0", "cscroll1", "scroll0", "scroll1", "tracker","crossbow","jacko", "pouchbow","orbg"];
 	
 	
     for (let i = 0; i < 42; i++) {
