2d815ee7faddac8104c16c9dfdbbead0d2dbc5a5
6gunlaZe <untilyouleesin@gmail.com>
2025-03-26 20:21:49 +0700

    Update ALLinOne_6gunlaZe_fram.js

--- ALLinOne_6gunlaZe_fram.js
+++ ALLinOne_6gunlaZe_fram.js
@@ -547,7 +547,7 @@ function scare() {
 
     for (id in parent.entities) {
         var current = parent.entities[id];
-        if (character.hp <5000 && current.target == character.name) {
+        if ((character.hp <5000 || smart.moving ) && current.target == character.name) {
             mobnum++;
             targetedForMoreThanOneSecond = true;
         }
