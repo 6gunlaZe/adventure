864407f7e26b7e51690431aa8b4369561b94a582
6gunlaZe <untilyouleesin@gmail.com>
2025-04-12 19:36:41 +0700

    Update ALLinOne_6gunlaZe_fram.js

--- ALLinOne_6gunlaZe_fram.js
+++ ALLinOne_6gunlaZe_fram.js
@@ -99,17 +99,22 @@ async function handleHome() {
 	    return
     }
 
-    if(  parent?.S?.wabbit.live && !character?.s?.easterluck  ) {
+    if (parent?.S?.wabbit?.live && !character?.s?.easterluck) {
         let wabbit = parent.S.wabbit;
-        if(wabbit && wabbit.live && !smart.moving) {
-            smart_move({ x: wabbit.x, y: wabbit.y, map: wabbit.map }).then(() => {
-                let target_monster = get_nearest_monster({ type : "wabbit" });
+        if (wabbit && wabbit.live && !smart.moving) {
+            try {
+                await smart_move({ x: wabbit.x, y: wabbit.y, map: wabbit.map });
+                let target_monster = get_nearest_monster({ type: "wabbit" });
                 if (target_monster) {
                     change_target(target_monster);
                 }
-            });
+            } catch (error) {
+                console.log("Không thể di chuyển đến wabbit, sử dụng 'use_town'");
+                await smart_move(destination);
+            }
+            game_log("Đang di chuyển đến wabbit");
         }
-	    return
+        return;
     }
 
     if (!smart.moving) {
