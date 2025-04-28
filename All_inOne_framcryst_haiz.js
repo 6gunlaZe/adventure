9e663b2a55332e3c9d52d3bec5caf4df0df73da0
6gunlaZe <untilyouleesin@gmail.com>
2025-04-29 00:49:46 +0700

    Update All_inOne_framcryst_haiz.js

--- All_inOne_framcryst_haiz.js
+++ All_inOne_framcryst_haiz.js
@@ -505,7 +505,7 @@ setInterval(function() {
         return;
     }
 
-    let itemsToExclude = ["elixirluck","candypop","hboots","cryptkey","hpot0", "mpot0","hpot1", "mpot1", "elixirint0","elixirstr0","elixirdex0","elixirint1","elixirstr1","elixirdex1", "luckbooster", "goldbooster", "xpbooster", "pumpkinspice", "xptome","cscroll0", "cscroll1", "scroll0", "scroll1", "jacko","tracker","mittens","xgloves","exoarm","hhelmet","helmet1","wbasher", "basher","bataxe","sweaterhs","tigerstone"];
+    let itemsToExclude = ["elixirluck","candypop","hboots","cryptkey","hpot0", "mpot0","hpot1", "mpot1", "elixirint0","elixirstr0","elixirdex0","elixirint1","elixirstr1","elixirdex1", "luckbooster", "goldbooster", "xpbooster", "pumpkinspice", "xptome","cscroll0", "cscroll1", "scroll0", "scroll1", "jacko","tracker","mittens","xgloves","exoarm","hhelmet","helmet1","wbasher", "basher","bataxe","sweaterhs","tigerstone","fury","rabbitsfoot",];
 
     for (let i = 0; i < 42; i++) {
         const item = character.items[i];
