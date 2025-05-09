98875d0189b3854b1907dd8d68840911303128cb
6gunlaZe <untilyouleesin@gmail.com>
2025-05-09 21:01:28 +0700

    Update ALLinOne_6gunlaZe_fram.js

--- ALLinOne_6gunlaZe_fram.js
+++ ALLinOne_6gunlaZe_fram.js
@@ -169,7 +169,7 @@ const { targets, inRange: monstersInRangeList , characterRange:  monsterscharact
 //game_log("monstersInRangeList.length" +monstersInRangeList.length)		
 //game_log("characterRange" +monsterscharacterRange.length)		
 
-            if( (leader && leader.hp < 8000) || (healerr && healerr.hp < 4000)  ){
+            if( (leader && leader.hp < 8000) || (healerr && healerr.hp < 5000)  ){
 		weaponSet("heal");
             const possibleTargets = [leader, healerr].filter(t => t); // bỏ null
             let healTarget = getLowestHpPercentTarget(possibleTargets);
