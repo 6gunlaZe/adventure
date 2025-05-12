ccc350750859575157838db4aa919c0640895ae8
6gunlaZe <untilyouleesin@gmail.com>
2025-05-12 21:40:32 +0700

    Update 28_2-buff-main.js

--- 28_2-buff-main.js
+++ 28_2-buff-main.js
@@ -714,7 +714,7 @@ const rangeBuffer = 60;
 const calcRadius = 300;
 
 // Types of monsters we want to avoid
-const avoidTypes = ["a0","a1","a2", "a3","a4","a6", "a7","a8","a9","vbat"];
+const avoidTypes = ["a0","a1","a2", "a3","a4","a5","a6", "a7","a8","a9","vbat"];
 
 const avoidPlayers = false; // Set to false to not avoid players at all
 const playerBuffer = 0; // Additional range around players
