248cb312a6c43b1d1feb2c8f07559b79ba80503b
6gunlaZe <untilyouleesin@gmail.com>
2025-05-14 22:04:28 +0700

    Update 28_2-buff-main.js

--- 28_2-buff-main.js
+++ 28_2-buff-main.js
@@ -570,7 +570,7 @@ function skill_scare() {
 	
 if (is_on_cooldown("scare")) 
 {
-	changeitem({ slot: "orb", name : "orbg", level : 3 });
+	changeitem({ slot: "orb", name : "tigerstone", level : 3 });
 }
 	
 if (character.targets == 0) {
