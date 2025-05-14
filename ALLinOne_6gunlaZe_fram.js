ea1d6b9e3ac37a509030b7d7ef754a229f94ade9
6gunlaZe <untilyouleesin@gmail.com>
2025-05-15 00:32:12 +0700

    Update ALLinOne_6gunlaZe_fram.js

--- ALLinOne_6gunlaZe_fram.js
+++ ALLinOne_6gunlaZe_fram.js
@@ -203,7 +203,7 @@ const { targets, inRange: monstersInRangeList , characterRange:  monsterscharact
                 delay = ms_to_next_skill("attack");
 		    
             } else if (targets.length > 0 && targets.length < 3 ) {
-		       if ( (leader && leader.hp < 14000) || (healerr && healerr.hp < 6000) )
+		       if ( (leader && leader.hp < 13000) || (healerr && healerr.hp < 6000) )
                            {
 		weaponSet("heal");
             const possibleTargets1 = [leader, healerr].filter(t => t); // bỏ null
