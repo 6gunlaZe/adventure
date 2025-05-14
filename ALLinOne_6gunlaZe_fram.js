88130772b02d7b2d33410a85074adee03c92ce10
6gunlaZe <untilyouleesin@gmail.com>
2025-05-14 21:41:39 +0700

    Update ALLinOne_6gunlaZe_fram.js

--- ALLinOne_6gunlaZe_fram.js
+++ ALLinOne_6gunlaZe_fram.js
@@ -1501,7 +1501,7 @@ const rangeBuffer = 90;
 const calcRadius = 300;
 
 // Types of monsters we want to avoid
-const avoidTypes = ["a0","a1","a2","a3","a4","a5","a6", "a7","a8","a9","vbat","stompy","skeletor","crabxx"];
+const avoidTypes = ["a0","a1","a2","a3","a4","a6", "a7","a8","a9","vbat","stompy","skeletor","crabxx"];
 
 const avoidPlayers = false; // Set to false to not avoid players at all
 const playerBuffer = 0; // Additional range around players
