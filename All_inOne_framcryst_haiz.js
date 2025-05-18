f2739293db5875f03359b21a4759075786752731
6gunlaZe <untilyouleesin@gmail.com>
2025-05-19 00:43:45 +0700

    Update All_inOne_framcryst_haiz.js

--- All_inOne_framcryst_haiz.js
+++ All_inOne_framcryst_haiz.js
@@ -1806,7 +1806,7 @@ setInterval(scare, 1500);  // Gọi lại scare() sau mỗi 1.5 giây
 
 ////////////////////////////////////////////////////////////////////////////////
 // Extra range to add to a monster's attack range to give more wiggle room
-const rangeBuffer = 22;
+const rangeBuffer = 25;
 
 // How far away we want to consider monsters
 const calcRadius = 300;
@@ -1915,7 +1915,7 @@ function avoidMobs() {
         }
 
         // Sau khi duyệt hết, chọn hướng tốt nhất và di chuyển
-        const movePoint = pointOnAngle(character, maxWeightAngle, 5); // Di chuyển mỗi lần 5 đơn vị
+        const movePoint = pointOnAngle(character, maxWeightAngle, 8); // Di chuyển mỗi lần 5 đơn vị
 
         if (!lastMove || new Date() - lastMove > 100) {
             lastMove = new Date();
