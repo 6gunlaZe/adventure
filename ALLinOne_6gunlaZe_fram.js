eeea02b817e2293e0f3cefe5b73b4f28da0d149a
6gunlaZe <untilyouleesin@gmail.com>
2025-05-19 21:40:19 +0700

    Update ALLinOne_6gunlaZe_fram.js

--- ALLinOne_6gunlaZe_fram.js
+++ ALLinOne_6gunlaZe_fram.js
@@ -635,7 +635,7 @@ if (check_viem_xung_quanh() == 1) targetedForMoreThanOneSecond = true;
 	
     for (id in parent.entities) {
         var current = parent.entities[id];
-        if ((  current.mtype == 'zapper0'  || character.hp < 6000 || (smart.moving && character.map != "crypt") ) && current.target == character.name) {
+        if ((  (current.mtype == 'zapper0'  && character.hp < 5000 )  || character.hp < 6000 || (smart.moving && character.map != "crypt") ) && current.target == character.name) {
             mobnum++;
             targetedForMoreThanOneSecond = true;
         }
