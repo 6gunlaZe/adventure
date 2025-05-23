cd9aa44f26adb7c152d345d7d1277a74e76eb879
6gunlaZe <untilyouleesin@gmail.com>
2025-05-23 18:26:08 +0700

    Update All_inOne.js

--- All_inOne.js
+++ All_inOne.js
@@ -740,7 +740,7 @@ function handleCleave(Mainhand, aoe, cc, stMaps, aoeMaps, tank) {
         type === "monster" &&
         visible &&
         !dead &&
-        distance(character, { x, y }) <= (G.skills.cleave.range + 10)
+        distance(character, { x, y }) <= (G.skills.cleave.range + 4)
     );
 
     const untargetedMonsters = monstersInRange.filter(({ target }) => !target)
