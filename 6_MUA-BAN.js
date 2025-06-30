c50204d9d72d14b1957c72a69ecc4dbc0878f904
6gunlaZe <untilyouleesin@gmail.com>
2025-06-30 19:54:44 +0700

    Update 6_MUA-BAN.js

--- 6_MUA-BAN.js
+++ 6_MUA-BAN.js
@@ -2162,7 +2162,7 @@ function skill_scare() {
 	
 if (is_on_cooldown("scare")) 
 {
-	changeitem({ slot: "orb", name : "orbg", level : 1 });
+	changeitem({ slot: "orb", name : "orbg", level : 3 });
 }
 	
 if (character.targets == 0) {
