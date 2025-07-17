ab2c412670d1e5011f4776ce08ef515f9983e11d
6gunlaZe <untilyouleesin@gmail.com>
2025-07-17 19:17:22 +0700

    Update 28_2-buff-main.js

--- 28_2-buff-main.js
+++ 28_2-buff-main.js
@@ -1355,15 +1355,18 @@ const mobsInRange = Object.values(parent.entities).filter(entity =>
 const physicalMobs = mobsInRange.filter(mob => mob.damage_type === "physical");
 const magicalMobs = mobsInRange.filter(mob => mob.damage_type === "magical");
 // Tách theo máu
-const lowHpMobs = mobsInRange.filter(mob =>
-    mob.hp < 6000 &&
-    mob.target === character.name &&
-    leader &&
-    distance(character, leader) <= 100 &&
-    mob.mtype !== "nerfedmummy" &&
-    mob.mtype !== "nerfedbat"
-);
-
+const lowHpMobs = mobsInRange.filter(mob => {
+    const hpThreshold = mob.max_hp >= 800000 ? 55000 :
+                        mob.max_hp >= 200000 ? 35000 : 7000;
+    return (
+        mob.hp < hpThreshold &&
+        mob.target === character.name &&
+        leader &&
+        distance(character, leader) <= 250 &&
+        mob.mtype !== "nerfedmummy" &&
+        mob.mtype !== "nerfedbat"
+    );
+});
 	
 
 	if (currentTime - eTime < 50)return
