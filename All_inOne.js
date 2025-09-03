cc0f700d8024bbd32370537ac8823483f1c1314d
6gunlaZe <untilyouleesin@gmail.com>
2025-09-03 23:57:50 +0700

    Update All_inOne.js

--- All_inOne.js
+++ All_inOne.js
@@ -1384,7 +1384,7 @@ if (
     !is_on_cooldown("agitate") &&
     mobsInRange.length >= 3 &&
     untargetedMobs.length >= 3 &&
-    tank && !tank.rip && distance(character, tank) < 180 && 
+    (   ( tank && tank.name == "Ynhi" && !tank.rip && distance(character, tank) < 180 )  ||  (tank && f1 && tank.name != "Ynhi")     ) && 
     character.hp > 14000 && character.mp > 800
 ) {
     const porc = get_nearest_monster({ type: "porcupine" });
