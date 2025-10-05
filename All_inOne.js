f376fef81cc6f32706156ccfbbae5b5397544c50
6gunlaZe <untilyouleesin@gmail.com>
2025-10-05 22:34:56 +0700

    Update All_inOne.js

--- All_inOne.js
+++ All_inOne.js
@@ -325,7 +325,7 @@ async function handleHome() {
 
     // 🔄 Khi đã ở đúng vị trí → quay vòng quanh trung tâm
         let center = locations[home][0];
-	const radius = 34;
+	const radius = 44;
 
         const currentTime = performance.now();
         const deltaTime = currentTime - lastUpdateTime;
