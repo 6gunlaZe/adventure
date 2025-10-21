9a21a3ad8de44bbe72452acebbab07c171619c4d
6gunlaZe <untilyouleesin@gmail.com>
2025-10-22 00:13:19 +0700

    Adjust target criteria for curse skill usage

--- 27_Ynhiii-buff.js
+++ 27_Ynhiii-buff.js
@@ -850,8 +850,8 @@ const hpThreshold = e.max_hp >= 800000 ? 45000 :
             }		
 	
 	
-	var target1xc= get_nearest_monster1({comuctieu: 1 ,  nhonhat: 21000});  //nhonhat : 41000   lonnhat: 1
-		    if(target1xc&& character.mp > 4500 && !is_on_cooldown("curse") &&  !target1xc.s["cursed"] && character.map != "winter_instance" && target1xc.hp > 20000 && distance(character, target1xc) < 70 )
+	var target1xc= get_nearest_monster1({comuctieu: 1 ,  nhonhat: 30001});  //nhonhat : 41000   lonnhat: 1
+		    if(target1xc&& character.mp > 5500 && !is_on_cooldown("curse") &&  !target1xc.s["cursed"] && character.map != "winter_instance" && target1xc.hp > 30000 && distance(character, target1xc) < 40 )
             {
                 use_skill("curse", target1xc);
 				// game_log("curse - low!!!!!!");
