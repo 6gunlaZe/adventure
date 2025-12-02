d220ccd4b6e9aafc09b2d0939ab0975eeaef63c4
6gunlaZe <untilyouleesin@gmail.com>
2025-12-02 20:22:32 +0700

    Update ALLinOne_6gunlaZe_fram.js

--- ALLinOne_6gunlaZe_fram.js
+++ ALLinOne_6gunlaZe_fram.js
@@ -26,7 +26,7 @@ const locations = {
     crab: [{ x: -11840, y: -37 }],
     ent: [{ x: -420, y: -1960 }],
     fireroamer: [
-  { type: "farm", x: -30, y: -800, map: "desertland" },
+  { type: "farm", x: 80, y: -800, map: "desertland" },
   { type: "safe", x: -30, y: -800, map: "desertland" }
     ],	
     ghost: [
@@ -147,8 +147,8 @@ setTimeout(eventer, 6000);
 
 
 
-//const mode_follow_haiz = false;
- const mode_follow_haiz = true; // nếu muốn quay quanh haiz ✅ Công tắc follow haiz
+   const mode_follow_haiz = false;
+ //const mode_follow_haiz = true; // nếu muốn quay quanh haiz ✅ Công tắc follow haiz
 
 async function handleHome() {
 if (smart.moving) return;
