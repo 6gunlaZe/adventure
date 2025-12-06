7b6c91bbebd4f109309c1c112b6f1e2c177a3150
6gunlaZe <untilyouleesin@gmail.com>
2025-12-06 20:36:53 +0700

    Update ALLinOne_6gunlaZe_fram.js

--- ALLinOne_6gunlaZe_fram.js
+++ ALLinOne_6gunlaZe_fram.js
@@ -30,7 +30,7 @@ const locations = {
   { type: "safe", x: -30, y: -800, map: "desertland" }
     ],	
     fireroamer: [
-  { type: "farm", x: 280, y: -1017, map: "desertland" },
+  { type: "farm", x: 400, y: -960, map: "desertland" },
   { type: "safe", x: 470, y: -960, map: "desertland" }
     ],	
     ghost: [
