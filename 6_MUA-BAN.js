147dc10d54e565ddb12704b905013d864b48278a
6gunlaZe <untilyouleesin@gmail.com>
2025-12-07 22:11:27 +0700

    Update 6_MUA-BAN.js

--- 6_MUA-BAN.js
+++ 6_MUA-BAN.js
@@ -171,7 +171,10 @@ bcape: {bank: "items1", level: -1, quantity: 1},
 bowofthedead: {bank: "items1", level: -1, quantity: 1},
 oozingterror: {bank: "items1", level: -1, quantity: 1},	
 firebow: {bank: "items1", level: 8, quantity: 1},	
+mshield: {bank: "items1", level: 6, quantity: 1},	
+supermittens: {bank: "items1", level: 4, quantity: 1},	
 
+	
 	
 	stramulet: {bank: "items7", level: -1, quantity: 1} ,
 	intamulet: {bank: "items7", level: -1, quantity: 1},
