cad3d6528baab6f4521b69adcf43cf48f48133fd
6gunlaZe <untilyouleesin@gmail.com>
2026-01-14 17:31:05 +0700

    Update ALLinOne_6gunlaZe_fram.js

--- ALLinOne_6gunlaZe_fram.js
+++ ALLinOne_6gunlaZe_fram.js
@@ -1759,7 +1759,7 @@ setInterval(function() {
     }
 
 
-    let itemsToExclude = ["coat","pumpkinspice","mittens","supermittens","snowball","hpot0", "mpot0","hpot1", "mpot1", "elixirint0","elixirstr0","elixirdex0","elixirint1","elixirstr1","elixirdex1", "luckbooster", "goldbooster", "xpbooster", "pumpkinspice", "xptome","cscroll0", "cscroll1", "scroll0", "scroll1", "tracker","crossbow","jacko", "pouchbow","orbg","cupid"];
+    let itemsToExclude = ["pants","coat","pumpkinspice","mittens","supermittens","snowball","hpot0", "mpot0","hpot1", "mpot1", "elixirint0","elixirstr0","elixirdex0","elixirint1","elixirstr1","elixirdex1", "luckbooster", "goldbooster", "xpbooster", "pumpkinspice", "xptome","cscroll0", "cscroll1", "scroll0", "scroll1", "tracker","crossbow","jacko", "pouchbow","orbg","cupid"];
 	
 	
     for (let i = 0; i < 42; i++) {
