573ab6e46081b38208167c78eed46a8b29a31346
6gunlaZe <untilyouleesin@gmail.com>
2026-01-18 22:30:30 +0700

    Update All_inOne.js

--- All_inOne.js
+++ All_inOne.js
@@ -1337,7 +1337,7 @@ if (
 	     let target2 = null;
 	    var bossarmy=["icegolem", "franky" , "crabxx" ]; 
 	    	    var mob=["phoenix111", "jr","greenjr", "mvampire","snowman","bgoo","rgoo","wabbit"];
-                    var mob2=["stompy", "skeletor","gbluepro","gredpro","ggreenpro", "xmagefz","xmagefi","xmagefn","xmagex","mrgreen","mrpumpkin", ]; //boss mạnh cần có healter
+                    var mob2=["stompy", "skeletor","gbluepro","ggreenpro", "gredpro", "xmagefz","xmagefi","xmagefn","xmagex","mrgreen","mrpumpkin", ]; //boss mạnh cần có healter
     
 // Kiểm tra xem target có thuộc trong bossarmy không
 if (!nearest && events){	  
