8e3a4235521ef5529245ffbbd1806d4b61dfc6e7
6gunlaZe <untilyouleesin@gmail.com>
2026-01-23 15:23:56 +0700

    Update ALLinOne_6gunlaZe_fram.js

--- ALLinOne_6gunlaZe_fram.js
+++ ALLinOne_6gunlaZe_fram.js
@@ -643,7 +643,7 @@ const equipmentSets = {
     boom: [
 
        // { itemName: "firebow", slot: "mainhand", level: 10, l: "l" },
-        { itemName: "bowofthedead", slot: "mainhand", level: 10, l: "l" },
+        { itemName: "pouchbow", slot: "mainhand", level: 10, l: "l" },
        // { itemName: "crossbow", slot: "mainhand", level: 8, l: "l" },
         //{ itemName: "pouchbow", slot: "mainhand", level: 9, l: "l" },
         { itemName: "alloyquiver", slot: "offhand", level: 8, l: "l" },
