f6013a90017d6a878c966b1709c401eccd64c64c
6gunlaZe <untilyouleesin@gmail.com>
2026-01-24 22:46:02 +0700

    Update ALLinOne_6gunlaZe_fram.js

--- ALLinOne_6gunlaZe_fram.js
+++ ALLinOne_6gunlaZe_fram.js
@@ -1995,7 +1995,7 @@ setInterval(function() {
     }
 
 
-    let itemsToExclude = ["poison","pants","coat","pumpkinspice","mittens","supermittens","snowball","hpot0", "mpot0","hpot1", "mpot1", "elixirint0","elixirstr0","elixirdex0","elixirint1","elixirstr1","elixirdex1", "luckbooster", "goldbooster", "xpbooster", "pumpkinspice", "xptome","cscroll0", "cscroll1", "scroll0", "scroll1", "tracker","crossbow","jacko", "pouchbow","orbg","cupid"];
+    let itemsToExclude = ["fieldgen0","frozenkey","spiderkey","poison","pants","coat","pumpkinspice","mittens","supermittens","snowball","hpot0", "mpot0","hpot1", "mpot1", "elixirint0","elixirstr0","elixirdex0","elixirint1","elixirstr1","elixirdex1", "luckbooster", "goldbooster", "xpbooster", "pumpkinspice", "xptome","cscroll0", "cscroll1", "scroll0", "scroll1", "tracker","crossbow","jacko", "pouchbow","orbg","cupid"];
 	
 	
     for (let i = 0; i < 42; i++) {
