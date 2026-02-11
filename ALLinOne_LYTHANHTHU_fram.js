ce3a10e9f89f192ee1c459425dd1d3548cda618b
6gunlaZe <untilyouleesin@gmail.com>
2026-02-11 22:18:50 +0700

    Update ALLinOne_LYTHANHTHU_fram.js

--- ALLinOne_LYTHANHTHU_fram.js
+++ ALLinOne_LYTHANHTHU_fram.js
@@ -90,7 +90,7 @@ const locations = {
 
 const f1111 = 'haiz';  ///tank fram check f1 có mới ra chỗ fram 
 
-const home = 'ent';
+const home = 'plantoid';
 const farmLocation = locations[home].find(p => p.type === "farm");
 const mobMap = farmLocation?.map || "main"; // fallback nếu không có
 
