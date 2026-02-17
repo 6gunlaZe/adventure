00cf582d7466c2e66b1b30d7c6a5d52a9d9d8914
6gunlaZe <untilyouleesin@gmail.com>
2026-02-17 21:38:36 +0700

    Update 6_MUA-BAN.js

--- 6_MUA-BAN.js
+++ 6_MUA-BAN.js
@@ -1878,7 +1878,7 @@ function compound_items() {
 
 // ====== DANH SÁCH ITEM MUỐN MUA ======
 var craftList = [
-    "wattire","wcap","wbreeches","wgloves","wshoes","smoke111",
+    "smoke111",
     "cscale","x0","x1","x2","x3","x4","x5","x6","x7","x8",
     "cshell","shield","gcape","sparkstaff",
     "candy1","candy0","carrot",
