c32cf1d1d8b78a6afc698b21f869b480f205e35d
6gunlaZe <untilyouleesin@gmail.com>
2026-03-03 12:57:35 +0700

    Decrease levels of fireblade and firebow to 7

--- 6_MUA-BAN.js
+++ 6_MUA-BAN.js
@@ -196,9 +196,9 @@ var bankItems = {
   bcape: [{ bank: "items1", level: -1, quantity: 1 }],
   bowofthedead: [{ bank: "items1", level: -1, quantity: 1 }],
   oozingterror: [{ bank: "items1", level: -1, quantity: 1 }],
-  fireblade: [{ bank: "items1", level: 8, quantity: 1 }],
+  fireblade: [{ bank: "items1", level: 7, quantity: 1 }],
 
-  firebow: [{ bank: "items1", level: 8, quantity: 1 }],
+  firebow: [{ bank: "items1", level: 7, quantity: 1 }],
   mshield: [{ bank: "items1", level: 6, quantity: 1 }],
   supermittens: [{ bank: "items1", level: 4, quantity: 1 }],
   orbofdex: [{ bank: "items1", level: 2, quantity: 1 }],
