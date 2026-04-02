9e50fc23ef79c1eac612884fe032273b6108a8c4
6gunlaZe <untilyouleesin@gmail.com>
2026-04-02 23:14:19 +0700

    Update ALLinOne_magic_nhiY.js

--- ALLinOne_magic_nhiY.js
+++ ALLinOne_magic_nhiY.js
@@ -753,7 +753,7 @@ if (!is_on_cooldown("energize")) {
         if (!is_in_range(rangerObj, "energize")) continue;
         if (rangerObj.rip) continue;
 
-        let mp_cap = rangerObj.max_mp - 1500;
+        let mp_cap = rangerObj.max_mp - 800;
         let ranger_need = mp_cap - rangerObj.mp;
 
         let mage_can_give = character.mp - 400;
