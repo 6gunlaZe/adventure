2af5ef3f7fc5078aa84e0dd7fee14335518f913a
6gunlaZe <untilyouleesin@gmail.com>
2026-04-02 07:26:55 +0700

    Update 6_MUA-BAN.js

--- 6_MUA-BAN.js
+++ 6_MUA-BAN.js
@@ -1848,7 +1848,7 @@ var combineWhitelist =
 		orbofdex: 1,
 		orbofint: 1,
 		orba: 2,
-		//ringsj: 4,
+		ringsj: 4,
 
 
 		
@@ -1930,7 +1930,7 @@ function compound_items() {
 
 // ====== DANH SÁCH ITEM MUỐN MUA ======
 var craftList = [
-    "smoke","weaponbox","intbelt","strbelt",
+    "smoke","weaponbox","intbelt","strbelt","ringsj",
     "cscale","x0","x1","x2","x3","x4","x5","x6","x7","x8",
     "cshell","shield","gcape","sparkstaff",
     "candy1","candy0","carrot",
