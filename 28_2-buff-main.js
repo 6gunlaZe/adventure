40ec89b8a4bc20e9a9047a2a9255985cb39c3a68
6gunlaZe <untilyouleesin@gmail.com>
2026-04-14 01:22:06 +0700

    Update 28_2-buff-main.js

--- 28_2-buff-main.js
+++ 28_2-buff-main.js
@@ -1377,7 +1377,7 @@ async function equipBatch(data) {
 
         for (let j = 0; j < parent.character.items.length; j++) {
             const item = parent.character.items[j];
-            if (item && item.name === itemName && item.level === level && item.l === l) {
+            if (item && item.name === itemName && item.level === level &&  (item.l === l || l === undefined) ) {
                 validItems.push({ num: j, slot: slot });
                 break;
             }
