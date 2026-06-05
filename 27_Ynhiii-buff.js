79c40a86d2de74ccf2b48bfc4e8f4f1641812e0c
6gunlaZe <untilyouleesin@gmail.com>
2026-06-05 23:39:03 +0700

    Update 27_Ynhiii-buff.js

--- 27_Ynhiii-buff.js
+++ 27_Ynhiii-buff.js
@@ -1611,7 +1611,7 @@ async function handleZap() {
             );
 
             // Step 2: Use zapper skill if conditions are met
-		if (character.targets <= soluongTOIDA && character.hp/character.max_hp > 0.75 && haiz && haiz.hp > 12700 && haiz.mp > 200 && distance(character, haiz) < 120 && gun )
+		if (character.targets <= soluongTOIDA && character.hp/character.max_hp > 0.75 && haiz && haiz.hp > 12700 && haiz.mp > 200 && distance(character, haiz) < 120 && (gun || crepp == "xscorpion" ) )
 		{
             if (entities.length > 0 && !is_on_cooldown("zapperzap") && character.mp > G?.skills?.zapperzap?.mp + 4250 && (character.slots.ring1?.name == "zapper" || character.slots.ring2?.name == "zapper" )  ) {
                 for (const entity of entities) {
