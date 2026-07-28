0457360938caa377f3aa16768ad29ee04c93d1d4
6gunlaZe <untilyouleesin@gmail.com>
2026-07-28 22:40:08 +0700

    Update 27_Ynhiii-buff.js

--- 27_Ynhiii-buff.js
+++ 27_Ynhiii-buff.js
@@ -1023,7 +1023,7 @@ if (franky_entity && franky_entity.target && franky_entity.target !== "haiz" && 
             let shouldAbsorb = false;
 
             // === Player yếu / quan trọng ===
-            if (player.hp < 7000 || name === "6gunlaZe" || name === "tienV" || name === "LyThanhThu" || name === "MuaBan" ) {
+            if (player.hp < 7000 || name === "6gunlaZe" || name === "tienV" || name === "LyThanhThu" || name === "MuaBan" || name === "nhiY" ) {
                 score += 50;
                 shouldAbsorb = true;
             }
