3bf3dbec4aa5bd9705eafb1cb9a409eb69a594d3
6gunlaZe <untilyouleesin@gmail.com>
2026-08-17 12:26:59 +0700

    Update 27_Ynhiii-buff.js

--- 27_Ynhiii-buff.js
+++ 27_Ynhiii-buff.js
@@ -329,7 +329,7 @@ setInterval(partyheal_logic, 600);
 let is_using_potion = false;
 
 function use_hp_or_mp1() {
-    if (is_using_potion) return;
+    if (is_using_potion || character.c.town ) return;
 
     try {
         let skill;
