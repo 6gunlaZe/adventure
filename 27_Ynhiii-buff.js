bc182f0005d43bb3550b2464305fcb6d3d8b57dd
6gunlaZe <untilyouleesin@gmail.com>
2026-08-18 21:00:27 +0700

    Update 27_Ynhiii-buff.js

--- 27_Ynhiii-buff.js
+++ 27_Ynhiii-buff.js
@@ -1951,7 +1951,7 @@ async function ZapCase() {
             entities.sort((a, b) => (a.target ? 1 : -1));
 			
             //logic phụ giửi lệnh dẫn quái ent về đánh
-            if ( Date.now()-lastEntRequest>150000 && isFullTeam && character.hp/character.max_hp > 0.75 && entcheck == 0 && character.map == "desertland" && !smart.moving && (crepp == "fireroamer" || crepp == "plantoid" ) && character.targets > 6 && !parent.S.franky && !parent.S.crabxx )
+            if ( Date.now()-lastEntRequest>150000 && isFullTeam && character.hp/character.max_hp > 0.75 && entcheck == 0 && character.map == "desertland" && !smart.moving && (crepp == "fireroamer" || crepp == "plantoid111" ) && character.targets > 6 && !parent.S.franky && !parent.S.crabxx )
 			{
 				send_cm("MuaBan","ent");
                 lastEntRequest=Date.now();
