de0765a0c1c0442670d1914e54f380eff360fcbf
6gunlaZe <untilyouleesin@gmail.com>
2026-08-26 23:52:06 +0700

    Update All_inOne.js

--- All_inOne.js
+++ All_inOne.js
@@ -130,7 +130,7 @@ const locations = {
 	
 };
 
-const home = 'mole';
+const home = 'targetron';
 const farmLocation = locations[home].find(p => p.type === "farm");
 const mobMap = farmLocation?.map || "main"; // fallback nếu không có
 
