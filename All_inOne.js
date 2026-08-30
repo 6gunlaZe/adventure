1d8af9c5b96be6a600158476a95beb9600851805
6gunlaZe <untilyouleesin@gmail.com>
2026-08-30 20:17:32 +0700

    Update All_inOne.js

--- All_inOne.js
+++ All_inOne.js
@@ -1917,8 +1917,9 @@ function handleWeaponSwap(stMaps, aoeMaps, Mainhand, offhand) {
         }
     }
 
-	let targetHighArmor = get_targeted_monster()?.armor > 300;
-
+const targetNow = get_targeted_monster();
+const targetHighArmor = targetNow?.armor > 300;
+	
     // Kiểm tra hiện tại đang dùng mana set
     const isManaSet = character.slots?.chest?.name === "tshirt9";
 
@@ -1957,7 +1958,7 @@ function handleWeaponSwap(stMaps, aoeMaps, Mainhand, offhand) {
         if (!defSafeSince) defSafeSince = currentTime;
 
         // Đủ thời gian an toàn → gỡ phòng thủ / check lại mỗi 40s
-        if ((currentTime - defSafeSince >= 3000 && character.slots.helmet?.name === "xhelmet") || (currentTime - defSafeSince >= 40000) ) {
+        if ((currentTime - defSafeSince >= 3000 && character.slots.helmet?.name === "xhelmet") || (currentTime - defSafeSince >= 40000 && targetNow) ) {
             checkdef = 0;
             defSafeSince = null;
             eTime = currentTime;
