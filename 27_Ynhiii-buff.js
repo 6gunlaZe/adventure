f627cbda91c5dcbcb661b9db68eaf870c1f28486
6gunlaZe <untilyouleesin@gmail.com>
2026-09-02 00:50:02 +0700

    Update 27_Ynhiii-buff.js

--- 27_Ynhiii-buff.js
+++ 27_Ynhiii-buff.js
@@ -1070,7 +1070,7 @@ function tryAbsorb() {
 
                 // 🎯 Trường hợp 2: Tối ưu riêng cho "haiz"
                 // Điều kiện: (Bị >= 3 quái Phép đánh HOẶC Máu haiz < 12000) &  HP > 10000
-                if ((checkmage >= 3 || player.hp < 12000) && name === "haiz" && character.hp > 10000) {
+                if ((checkmage >= 3 || player.hp < 13500) && name === "haiz" && character.hp > 10000) {
                     score += 20;
                     shouldAbsorb = true;
                 }
