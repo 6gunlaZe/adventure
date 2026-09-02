3b531fea5f51fadbdb246a861016377471c01c39
6gunlaZe <untilyouleesin@gmail.com>
2026-09-02 21:32:25 +0700

    Update All_inOne.js

--- All_inOne.js
+++ All_inOne.js
@@ -2335,9 +2335,9 @@ if (current.mtype === "ent" && current.target) {
             game_log(`🐷 Taunt ${freePullMob.mtype} (free pull)`, "#FF8800");
         }
     }
-// 🌲 ƯU TIÊN 4: Chỉ hút tối đa 1 con Ent từ Ynhi NẾU Warrior chưa bị con Ent nào đánh (checkENT === 0)
+// 🌲 ƯU TIÊN 4: Chỉ hút tối đa 1 con Ent từ Ynhi NẾU Warrior chưa bị con Ent nào đánh (checkENT === 0) và +1 con nếu đang có HARDSHELL
 else if (
-    checkENT <= 0 && character.hp > 15000 && character.mp > 1000 &&
+    (checkENT <= 0 || (checkENT <= 1 && character.s.hardshell ) ) && character.hp > 15000 && character.mp > 1000 &&
     hutENT && 
     !is_on_cooldown("taunt") && 
     is_in_range(hutENT, "taunt")
