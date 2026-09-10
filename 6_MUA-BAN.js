27d841b156c0ad9f5ec9b1fca2ea426845a8d120
6gunlaZe <untilyouleesin@gmail.com>
2026-09-10 16:32:59 +0700

    Update 6_MUA-BAN.js

--- 6_MUA-BAN.js
+++ 6_MUA-BAN.js
@@ -2011,7 +2011,7 @@ var craftList = [
     "smoke","weaponbox","intbelt","strbelt","spores","vitearring","ashleaf","gslime","voidthread","bwing","essenceofether","ijx","ectoplasm","bcandle","snakefang","embercore",
     "cscale","x0","x1","x2","x3","x4","x5","x6","x7","x8",
     "cshell","shield","gcape","sparkstaff","harbringer",
-    "candy1","candy0","carrot",
+    "candy1","candy0","carrot","confetti","firecrackers",
     "fallen","ink","bowofthedead"
     ,"dexearring","intring","intearring",
     "egg0","egg1","egg2","egg3","egg4","egg5","egg6","egg7","egg8",
