6c4786a72203cfdabc857475e0594422ab518ac7
6gunlaZe <untilyouleesin@gmail.com>
2026-09-14 12:16:44 +0700

    Update 6_MUA-BAN.js

--- 6_MUA-BAN.js
+++ 6_MUA-BAN.js
@@ -2026,7 +2026,7 @@ function compound_items() {
 var craftList = [
     "smoke","weaponbox","intbelt","strbelt","vitearring","gslime","voidthread","bwing","essenceofether","ijx","ectoplasm","bcandle","snakefang","embercore",
     "cscale","x0","x1","x2","x3","x4","x5","x6","x7","x8",
-    "cshell","shield","gcape","sparkstaff","harbringer","stormfeather","fireblade",
+    "cshell","gcape","sparkstaff","harbringer","stormfeather","fireblade",
     "candy1","candy0","carrot","firecrackers",
     "fallen","ink","bowofthedead"
     ,"dexearring","confetti",
