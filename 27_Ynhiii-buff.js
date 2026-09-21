5e7a4fb7b52325cc57ae4bb2d33f4f5ee8cadf71
6gunlaZe <untilyouleesin@gmail.com>
2026-09-22 02:52:39 +0700

    Reduce TEMPORAL_DELAY from 900 to 200

--- 27_Ynhiii-buff.js
+++ 27_Ynhiii-buff.js
@@ -2649,7 +2649,7 @@ let boquaTEMPORAL = 3; // bỏ qua một số lần để tránh trùng người
 // =========================
 const TEMPORAL_RADIUS = 270;   // bán kính tính quái quanh người
 const TEMPORAL_GAP = 5;        // hụt bao nhiêu quái thì dùng skill
-const TEMPORAL_DELAY = 900;    // delay trước khi cast
+const TEMPORAL_DELAY = 200;    // delay trước khi cast
 const TEMPORAL_COOLDOWN = 10000; // tối thiểu 10 giây giữa 2 lần cast
 
 // =========================
