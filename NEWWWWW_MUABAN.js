c84864974a4ef065fece6ca4ffeb608d58fdd23c
6gunlaZe <untilyouleesin@gmail.com>
2026-09-23 20:16:29 +0700

    Update NEWWWWW_MUABAN.js

--- NEWWWWW_MUABAN.js
+++ NEWWWWW_MUABAN.js
@@ -3,8 +3,18 @@ const MERCHANT = "MuaBan";
 const LEADER = "haiz";
 
 // Danh sách item rác sẽ tự động bán
-const TRASH_ITEMS = ["slimestaff","hpamulet","hpbelt","stinger","beewings","helmet","shoes","crabclaw","reefglass","mushroomstaff","ringsj","stramulet","whiteegg","vitscroll","smush",
-					 "dstones","frogt","seashell","oozingterror","basher","spear","dagger","pmace","hammer","rapier","sword","cclaw"];
+const TRASH_ITEMS = [
+  "alloyquiver", "bandages", "basher", "beewings", "blade1111", "broom", "bunnyelixir","cake", "candycandesword", "cape", "carrotsword", "carrotsword1111", "cave_reedscythe",
+  "cclaw", "coat", "coat1", "crabclaw", "cupid", "dagger", "daggerofthedead11111", "dstones", "eears", "ecape11", "eggnog","elixirdex0", "elixirdex1", "elixirdex2", "elixirint0", "elixirint1", "elixirint2",
+  "elixirpnres", "elixirstr0", "elixirstr1", "elixirstr2", "elixirvit0", "elixirvit1","elixirvit2", "epyjamas", "eslippers", "fieldgen0", "fireblade1111", "firecrackers",
+  "firestaff", "frankypants", "frogt", "gbow", "gloves", "gloves1", "gphelmet", "hammer","harmor", "hboots", "hgloves", "hhelmet", "hpamulet", "hpants", "hpbelt", "jacko",
+  "lantern", "lostearring", "lspores", "maceofthedead", "merry", "mittens", "mittens1111",
+  "mushroomstaff", "oozingterror", "ornamentstaff", "pants", "pants1", "partyhat","pclaw", "phelmet", "pinkie", "pmace", "pmaceofthedead", "poker", "pouchbow111",
+  "pstem", "pumpkinspice", "quiver", "rapier", "rednose", "reefglass", "rfangs", "ringsj","santasbelt", "seashell", "shoes", "shoes1", "skates", "skullamulet", "slimestaff",
+  "smoke111", "smush", "snowball111", "snowflakes", "sparkstaff", "spear", "spikedhelmet","sshield", "sstinger", "staffofthedead", "stinger", "stramulet", "strearring", "svenom",
+  "swifty", "sword", "swordofthedead", "t2bow", "t2quiver", "talkingskull", "throwingstars","tombkey", "tshirt0", "tshirt1", "tshirt2", "tshirt3", "vboots", "vgloves", "vitring",
+  "vitscroll", "wbasher", "wbook0", "wcap", "wbreeches", "wgloves", "whiteegg", "wshoes","wattire", "warmscarf", "xmace", "xmashat", "xmaspants", "xmasshoes", "xmassweater"
+];
 
 // 1. Khai báo danh sách các món đồ ưu tiên chế tạo
 const craftList = ["computer", "cloverstud","moonshardearring",];
