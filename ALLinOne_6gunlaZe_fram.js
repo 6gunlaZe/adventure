95486e27f1e5e3d7132422e53d2af51e93296835
6gunlaZe <untilyouleesin@gmail.com>
2026-09-24 02:06:26 +0700

    Update ALLinOne_6gunlaZe_fram.js

--- ALLinOne_6gunlaZe_fram.js
+++ ALLinOne_6gunlaZe_fram.js
@@ -2282,57 +2282,6 @@ setInterval(function() {
 
 
 
-setInterval(function() {
-	
-if (character.map == "crypt") return
-	
-		if(character.esize < 7 || !character.s.mluck || character.s.mluck.f !== "MuaBan")
-	{
-		send_cm("MuaBan", "full");
-		game_log("lay do !!!!!!");
-	}
-
-
-	let soluonghp = 0
-	let soluongmp = 0
-   /////////
-	        for (let i = 0; i < character.isize; i++) {
-            const item = character.items[i]
-            if (!item) continue // No item in this slot
-
-            if (item.name == "mpot1" ) {
-                // This is an item we want to use!
-                    soluongmp += item.q//tim ra vi tri mon do
-						// game_log("so luong  la "+soluongmp);
-
-            }
-            if (item.name == "hpot1" ) {
-                // This is an item we want to use!
-                    soluonghp += item.q//tim ra vi tri mon do
-						// game_log("so luong  la "+soluonghp);
-
-            }				
-			}
-	/////////		
-	
-	if( (soluonghp < 4000 ) )
-	{
-		send_cm("MuaBan", "hp");
-		game_log("re filll !!!!!!");
-	}
-		if( ( soluongmp < 9999) )
-	{
-		send_cm("MuaBan", "mp");
-		game_log("re filll !!!!!!");
-	}
-/////////////		
-
-	
-
-	
-}, 20000); 
-
-
 
 
 ////////////////////////////////////////////////////////
@@ -2986,6 +2935,7 @@ const COOLDOWN_MS = 60000;
 
 setInterval(() => {
     if (character.rip) return;
+    const MERCHANT = "MuaBan";
 
     const { esize, map, x, y, items, s } = character;
     const now = Date.now();
