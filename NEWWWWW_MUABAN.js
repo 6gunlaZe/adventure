659a5528abe9ebff16ccf6638abe0f075226decc
6gunlaZe <untilyouleesin@gmail.com>
2026-09-24 00:02:00 +0700

    Update NEWWWWW_MUABAN.js

--- NEWWWWW_MUABAN.js
+++ NEWWWWW_MUABAN.js
@@ -32,7 +32,7 @@ const EXCHANGE = { anniversarygift: 1, armorbox: 1, basketofeggs: 1, brownenvelo
 //  Luôn giữ lại các vật phẩm quan trọng trên túi đồ
 const IMPORTANT_ITEMS = [
         "scroll0", "scroll1", "scroll2","stand0","stand1",
-        "cscroll0", "cscroll1", "cscroll2", 
+        "cscroll0", "cscroll1", "cscroll2", "offeringp",
         "tracker", "computer", "supercomputer"
     ];
 
