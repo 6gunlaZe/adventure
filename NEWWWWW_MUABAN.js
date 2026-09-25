ee54101becd5003f81d312efebc141f7cf9bd9e2
6gunlaZe <untilyouleesin@gmail.com>
2026-09-25 19:35:30 +0700

    Update NEWWWWW_MUABAN.js

--- NEWWWWW_MUABAN.js
+++ NEWWWWW_MUABAN.js
@@ -354,7 +354,7 @@ const COMPOUND_RULES = [
     { items: ["cloverstud","moonshardearring","windbelt"], rules: [{ levels: [0], scroll: null, offering: null }, { levels: [1], scroll: "cscroll1", offering: "offeringp" }, ] },
     { items: ["spookyamulet","ftrinket"], rules: [{ levels: [0, 1, 2,], scroll: "cscroll1", offering: null },  ] },
     { items: ["mossheart"], rules: [{ levels: [0,], scroll: "cscroll1", offering: null },  ] },
-    { items: ["keepsakependant"], rules: [{ levels: [0,1], scroll: "cscroll1", offering: null },  ] },
+    { items: ["keepsakependant","guestbook",], rules: [{ levels: [0,1], scroll: "cscroll1", offering: null },  ] },
 
 ];
 
