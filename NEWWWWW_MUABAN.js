f616f54337b62abb0895f84a5044ce940e3a221b
6gunlaZe <untilyouleesin@gmail.com>
2026-09-25 19:32:55 +0700

    Update NEWWWWW_MUABAN.js

--- NEWWWWW_MUABAN.js
+++ NEWWWWW_MUABAN.js
@@ -7,14 +7,14 @@ const LEADER = "haiz";
 
 // Danh sách item rác sẽ tự động bán
 const TRASH_ITEMS = [
-  "alloyquiver", "bandages", "basher", "beewings", "blade1111", "broom", "bunnyelixir","cake", "candycandesword", "cape", "carrotsword", "carrotsword1111", "cave_reedscythe",
+  "bandages", "basher", "beewings", "blade1111", "broom", "bunnyelixir","cake", "candycandesword", "cape", "carrotsword", "carrotsword1111", "cave_reedscythe",
   "cclaw", "coat", "coat1", "crabclaw", "cupid", "dagger", "daggerofthedead11111", "dstones", "eears", "ecape11", "eggnog","elixirdex0", "elixirdex1", "elixirdex2", "elixirint0", "elixirint1", "elixirint2",
   "elixirpnres", "elixirstr0", "elixirstr1", "elixirstr2", "elixirvit0", "elixirvit1","elixirvit2", "epyjamas", "eslippers", "fieldgen0", "fireblade1111", "firecrackers",
   "firestaff", "frankypants", "frogt", "gbow", "gloves", "gloves1", "gphelmet", "hammer","harmor", "hboots", "hgloves", "hhelmet", "hpamulet", "hpants", "hpbelt", "jacko",
-  "lantern", "lostearring", "lspores", "maceofthedead", "merry", "mittens", "mittens1111","helmet1","helmet","shield",
+  "lantern", "lostearring", "lspores", "maceofthedead", "merry", "mittens", "mittens1111","helmet1","helmet","shield","sparkstaff","alloyquiver",
   "mushroomstaff", "oozingterror", "ornamentstaff", "pants", "pants1", "partyhat","pclaw", "phelmet", "pinkie", "pmace", "pmaceofthedead", "poker", "pouchbow111",
   "pstem", "pumpkinspice", "quiver", "rapier", "rednose", "reefglass", "rfangs", "ringsj","santasbelt", "seashell", "shoes", "shoes1", "skates", "skullamulet", "slimestaff",
-  "smoke111", "smush", "snowball111", "snowflakes", "sparkstaff", "spear", "spikedhelmet","sshield", "sstinger", "staffofthedead", "stinger", "svenom",
+  "smoke111", "smush", "snowball111", "snowflakes", "spear", "spikedhelmet","sshield", "sstinger", "staffofthedead", "stinger", "svenom",
   "swifty", "sword", "swordofthedead", "t2bow", "t2quiver", "talkingskull", "throwingstars","tombkey", "tshirt0", "tshirt1", "tshirt2", "tshirt3", "vboots", "vgloves", "vitring",
   "vitscroll", "wbasher", "wcap", "wbreeches", "wgloves", "whiteegg", "wshoes","wattire", "warmscarf", "xmace", "xmashat", "xmaspants", "xmasshoes", "xmassweater"
 ];
@@ -330,7 +330,7 @@ var upgradeWhitelistVIPP = {
 	group_weapon5: ["bataxe"],
 
 	group_rare1: ["handofmidas","hdagger","xboots","xgloves",],
-	group_rare2: ["sparkstaff1111",],
+	group_rare2: ["sparkstaff111",],
 	group_rare: ["xarmor111","t3bow","lmace"],
 	group_vip: ["vattire","vstaff"],
 	group_vip1: ["starkillers1111","vhammer111","vdagger","xhelmet"],
